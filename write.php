e5599d5e0f1850f837a27432a78607f7960008cc
kdsystem <kdsystem@gmail.com>
2018-06-16 11:41:57 +0300

    2.0
    server side parser

---
+++ write.php
@@ -0,0 +1,27 @@
+<?php
+ include 'keeneye_db.php';
+ @mysqli_query ($db, 'set character_set_results = "utf8"');
+ //mysql_query ($db, "set character_set_results = 'utf8'");
+ $car_id=$_GET['car_id'];
+ //3025.711000&lon=6001.979700
+ $llat=$_GET['lat'];
+ $llon=$_GET['lon'];
+ $degWhole = intval($llon/100);
+ $degDec = ($llon - $degWhole*100)/60;
+ $lon = $degWhole + $degDec;
+ //echo $lon;
+ $degWhole = intval($llat/100);
+ $degDec = ($llat - $degWhole*100)/60;
+ $lat = $degWhole + $degDec;
+ //echo "<br/>";
+ //echo $lat;
+ $data=date('Y-m-d H:i:s');
+
+ $query="INSERT into tracker SET longitude=".$lon.", latitude=".$lat.", car_id=".$car_id;
+ $res = mysqli_query($db, $query);
+
+ if(!mysqli_query($db, $query))
+    echo "error=".$query;
+  else
+    echo "OK";
+?>
